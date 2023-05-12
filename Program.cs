2217ec2292c40c598e3f14a233c88a6f74d7aa45
MarceloMena0902 <mmm0033334@est.univalle.edu>
2023-05-12 11:21:35 -0400

    primera version

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GITHUB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int s,i,n;
+            n= int.Parse(Console.ReadLine());
+           
+            for (s=1;s<=n;s++) 
+            {
+                for(i=1;i<=s;i++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
